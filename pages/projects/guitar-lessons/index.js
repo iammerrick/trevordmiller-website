dac46e0e56ded8ca54cbf1436b68a2a027ee3190
iammerrick <merrick.christensen@gmail.com>
2017-02-19 12:05:24 -0700

    feature: url-support

--- pages/projects/guitar-lessons/index.js
+++ pages/projects/guitar-lessons/index.js
@@ -101,10 +101,18 @@ const Selector = ({title, onClick, selectedItem, items}) => (
 
 class GuitarPatternsSelector extends Component {
 
-  state = {
+  static defaultProps = {
     selectedDegree: 'None',
     selectedType: 'singleNotes',
-    highlightedDegreesByString: false,
+  }
+
+  constructor() {
+    super(...arguments);
+    this.state = {
+      selectedDegree: this.props.selectedDegree,
+      selectedType: this.props.selectedType,
+      highlightedDegreesByString: this.getHighlightedDegreesByString(this.props.selectedType, this.props.selectedDegree), // tried to do this as initializer for > hour :-(
+    }
   }
 
   handleDegreeChange = (selectedDegree) => {
@@ -112,11 +120,16 @@ class GuitarPatternsSelector extends Component {
   }
 
   handleTypeChange = (selectedType) => {
-    this.setState({selectedType}, this.handleOptionsChange)
+    this.setState({selectedType}, () => this.handleOptionsChange)
   }
 
   handleOptionsChange = () => {
-    const {selectedType, selectedDegree} = this.state
+    this.setState({ 
+      highlightedDegreesByString: this.getHighlightedDegreesByString(this.state.selectedType, this.state.selectedDegree)
+    });
+  }
+
+  getHighlightedDegreesByString = (selectedType, selectedDegree) => {
 
     const intervalToDegree = interval => {
       const remainder = (selectedDegree + (interval - 1)) % 7
@@ -125,59 +138,57 @@ class GuitarPatternsSelector extends Component {
         : remainder
     }
 
-    this.setState({
-      highlightedDegreesByString: {
-        singleNotes: {
-          1: [selectedDegree],
-          2: [selectedDegree],
-          3: [selectedDegree],
-          4: [selectedDegree],
-          5: [selectedDegree],
-          6: [selectedDegree],
-        },
-        powerChords: {
-          3: [selectedDegree],
-          4: [selectedDegree, intervalToDegree(5)],
-          5: [selectedDegree, intervalToDegree(5)],
-          6: [selectedDegree],
-        },
-        basicChords: {
-          2: [intervalToDegree(3)],
-          3: [intervalToDegree(3), selectedDegree],
-          4: [selectedDegree, intervalToDegree(5)],
-          5: [intervalToDegree(5), selectedDegree],
-          6: [selectedDegree],
-        },
-        seventhChords: {
-          2: [intervalToDegree(3)],
-          3: [intervalToDegree(3), intervalToDegree(7)],
-          4: [intervalToDegree(7), intervalToDegree(3)],
-          5: [intervalToDegree(3), selectedDegree],
-          6: [selectedDegree],
-        },
-        ninthChords: {
-          2: [intervalToDegree(2)],
-          3: [intervalToDegree(2), intervalToDegree(7)],
-          4: [intervalToDegree(7), intervalToDegree(3)],
-          5: [intervalToDegree(3), selectedDegree],
-          6: [selectedDegree],
-        },
-        eleventhChords: {
-          2: [intervalToDegree(4)],
-          3: [intervalToDegree(4), intervalToDegree(7)],
-          4: [intervalToDegree(7), intervalToDegree(3)],
-          5: [intervalToDegree(3), selectedDegree],
-          6: [selectedDegree],
-        },
-        quartalChords: {
-          2: [intervalToDegree(3)],
-          3: [intervalToDegree(3), intervalToDegree(7)],
-          4: [intervalToDegree(7), intervalToDegree(4)],
-          5: [intervalToDegree(4), selectedDegree],
-          6: [selectedDegree],
-        },
-      }[selectedType],
-    })
+    return {
+      singleNotes: {
+        1: [selectedDegree],
+        2: [selectedDegree],
+        3: [selectedDegree],
+        4: [selectedDegree],
+        5: [selectedDegree],
+        6: [selectedDegree],
+      },
+      powerChords: {
+        3: [selectedDegree],
+        4: [selectedDegree, intervalToDegree(5)],
+        5: [selectedDegree, intervalToDegree(5)],
+        6: [selectedDegree],
+      },
+      basicChords: {
+        2: [intervalToDegree(3)],
+        3: [intervalToDegree(3), selectedDegree],
+        4: [selectedDegree, intervalToDegree(5)],
+        5: [intervalToDegree(5), selectedDegree],
+        6: [selectedDegree],
+      },
+      seventhChords: {
+        2: [intervalToDegree(3)],
+        3: [intervalToDegree(3), intervalToDegree(7)],
+        4: [intervalToDegree(7), intervalToDegree(3)],
+        5: [intervalToDegree(3), selectedDegree],
+        6: [selectedDegree],
+      },
+      ninthChords: {
+        2: [intervalToDegree(2)],
+        3: [intervalToDegree(2), intervalToDegree(7)],
+        4: [intervalToDegree(7), intervalToDegree(3)],
+        5: [intervalToDegree(3), selectedDegree],
+        6: [selectedDegree],
+      },
+      eleventhChords: {
+        2: [intervalToDegree(4)],
+        3: [intervalToDegree(4), intervalToDegree(7)],
+        4: [intervalToDegree(7), intervalToDegree(3)],
+        5: [intervalToDegree(3), selectedDegree],
+        6: [selectedDegree],
+      },
+      quartalChords: {
+        2: [intervalToDegree(3)],
+        3: [intervalToDegree(3), intervalToDegree(7)],
+        4: [intervalToDegree(7), intervalToDegree(4)],
+        5: [intervalToDegree(4), selectedDegree],
+        6: [selectedDegree],
+      },
+    }[selectedType];
   }
 
   render() {
@@ -199,46 +210,46 @@ class GuitarPatternsSelector extends Component {
         <div style={{
           marginBottom: spacing.medium,
         }}>
-          We can apply the in-key pattern to the guitar. Each dot on the fretboard below is a note that is "in-key". This entire pattern repeats up and down so the ends "connect". The pattern is not specific to any note or fret number - it can be shifted up or down depending on the "key" you are in. Scroll horizontally to view the entire pattern. Tap a "Degree" and "Type" to highlight specific notes or chords inside the pattern. The most common chord progressions in typical songs use primarily the 1, 4, 5, and 6 degree chords. Most common songs will resolve back to the 1 (Major) or 6 (Minor) degree chord. Once you find the "root" of a song, you can play any of the notes in the entire in-key pattern and it is likely to sound good.
-        </div>
+        We can apply the in-key pattern to the guitar. Each dot on the fretboard below is a note that is "in-key". This entire pattern repeats up and down so the ends "connect". The pattern is not specific to any note or fret number - it can be shifted up or down depending on the "key" you are in. Scroll horizontally to view the entire pattern. Tap a "Degree" and "Type" to highlight specific notes or chords inside the pattern. The most common chord progressions in typical songs use primarily the 1, 4, 5, and 6 degree chords. Most common songs will resolve back to the 1 (Major) or 6 (Minor) degree chord. Once you find the "root" of a song, you can play any of the notes in the entire in-key pattern and it is likely to sound good.
+      </div>
 
-        <div style={{
-          marginBottom: spacing.medium,
-        }}>
-          <GuitarPattern 
-            strings={strings}
-            highlightedDegreesByString={highlightedDegreesByString}
-          />
-        </div>
+      <div style={{
+        marginBottom: spacing.medium,
+      }}>
+      <GuitarPattern 
+        strings={strings}
+        highlightedDegreesByString={highlightedDegreesByString}
+      />
+    </div>
 
-        <Selector
-          title='Degree'
-          onClick={this.handleDegreeChange}
-          selectedItem={selectedDegree}
-          items={['None', 1, 2, 3, 4, 5, 6, 7]}
-        />
+    <Selector
+      title='Degree'
+      onClick={this.handleDegreeChange}
+      selectedItem={selectedDegree}
+      items={['None', 1, 2, 3, 4, 5, 6, 7]}
+    />
 
-        <Selector
-          title='Type'
-          onClick={this.handleTypeChange}
-          selectedItem={selectedType}
-          items={[
-            'singleNotes',
-            'powerChords',
-            'basicChords',
-            'seventhChords',
-            'ninthChords',
-            'eleventhChords',
-            'quartalChords',
-          ]}
-        />
+  <Selector
+    title='Type'
+    onClick={this.handleTypeChange}
+    selectedItem={selectedType}
+    items={[
+      'singleNotes',
+      'powerChords',
+      'basicChords',
+      'seventhChords',
+      'ninthChords',
+      'eleventhChords',
+      'quartalChords',
+    ]}
+  />
 
       </div>
     )
   }
 }
 
-export default () => (
+const GuitarLessonIndex = ({ selectedDegree, selectedType}) => (
   <Screen
     title='Guitar lessons'
     description='Diatonic (in-key) chord and scale diagrams to help you go beyond tabs and memorizing songs'
@@ -251,25 +262,32 @@ export default () => (
             <div style={{
               marginBottom: spacing.medium,
             }}>
-              In common music, most notes used are "in-key". Then, chords (multiple notes played at a time) are built from these in-key notes. There are only twelve notes in music, but they repeat in "octaves". Only seven of these notes are in-key. Below is what the pattern of in-key notes looks like. Scroll horizontally to view the entire pattern. This pattern repeats infinitely up and down so the ends "connect".
-            </div>
-
-            <GuitarPattern 
-              strings={[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1]]}
-              highlightedDegreesByString={{
-                1: [1, 3, 5, 6, 8, 10, 12],
-              }}
-            />
-
+            In common music, most notes used are "in-key". Then, chords (multiple notes played at a time) are built from these in-key notes. There are only twelve notes in music, but they repeat in "octaves". Only seven of these notes are in-key. Below is what the pattern of in-key notes looks like. Scroll horizontally to view the entire pattern. This pattern repeats infinitely up and down so the ends "connect".
           </div>
+
+          <GuitarPattern 
+            strings={[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1]]}
+            highlightedDegreesByString={{
+              1: [1, 3, 5, 6, 8, 10, 12],
+            }}
+          />
+
+      </div>
         ),
       },
       {
         title: 'The in-key guitar pattern',
         component: (
-          <GuitarPatternsSelector />
+          <GuitarPatternsSelector selectedDegree={selectedDegree} selectedType={selectedType} />
         ),
       },
     ]}
   />
 )
+
+GuitarLessonIndex.getInitialProps = ({query: { degree, type}}) => ({
+  selectedDegree: degree ? parseInt(degree) : undefined, 
+  selectedType: type,
+})
+
+export default GuitarLessonIndex;
