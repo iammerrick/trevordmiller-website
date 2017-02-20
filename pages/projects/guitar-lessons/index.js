77bb120beeb8845b644f15bfcf5b621490e28ee1
iammerrick <merrick.christensen@gmail.com>
2017-02-20 13:20:43 -0700

    fix formatting issues

--- pages/projects/guitar-lessons/index.js
+++ pages/projects/guitar-lessons/index.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react'
-import {startCase} from 'lodash'
-import {uiGroups, spacing, borderSizes} from '../../../components/utils/styleGuide'
+import React, { Component } from 'react';
+import { startCase } from 'lodash';
+import {
+  uiGroups,
+  spacing,
+  borderSizes,
+} from '../../../components/utils/styleGuide';
 import Link from 'next/Link';
-import Screen from '../../../components/Screen'
-import Heading from '../../../components/Heading'
-import Button from '../../../components/Button'
+import Screen from '../../../components/Screen';
+import Heading from '../../../components/Heading';
+import Button from '../../../components/Button';
 
-const getHighlightedDegreesByString = (selectedType, selectedDegree) => { 
+const getHighlightedDegreesByString = (selectedType, selectedDegree) => {
   const intervalToDegree = interval => {
-    const remainder = (selectedDegree + (interval - 1)) % 7
-    return remainder === 0
-      ? 7
-      : remainder
-  }
+    const remainder = (selectedDegree + (interval - 1)) % 7;
+    return remainder === 0 ? 7 : remainder;
+  };
 
-  return {
+  return ({
     singleNotes: {
       1: [selectedDegree],
       2: [selectedDegree],
@@ -64,115 +66,128 @@ const getHighlightedDegreesByString = (selectedType, selectedDegree) => {
       5: [intervalToDegree(4), selectedDegree],
       6: [selectedDegree],
     },
-  }[selectedType]
-}
+  })[selectedType];
+};
 
-
-const GuitarPattern = ({
-  strings,
-  highlightedDegreesByString,
-  noteSize = 20,
-  fretSize = 60,
-}) => (
-  <div style={{
-    marginTop: spacing.medium,
+const GuitarPattern = (
+  {
+    strings,
+    highlightedDegreesByString,
+    noteSize = 20,
+    fretSize = 60,
+  },
+) => (
+  <div
+    style={{
+      marginTop: spacing.medium,
       overflowX: 'scroll',
-  }}>
-  <div style={{
-    background: uiGroups.backgroundShade,
-      display: 'inline-block',
-  }}>
-  {strings.map((string, stringIndex) => (
-    <div 
-      key={stringIndex}
+    }}
+  >
+    <div
       style={{
-        display: 'flex',
+        background: uiGroups.backgroundShade,
+        display: 'inline-block',
       }}
     >
-      {string.map((degree, noteIndex) => (
-        <div 
-          key={noteIndex}
+      {strings.map((string, stringIndex) => (
+        <div
+          key={stringIndex}
           style={{
-            width: fretSize,
-              display: 'flex',
-              position: 'relative',
-              justifyContent: 'center',
-              alignItems: 'center',
-              paddingTop: spacing.xxsmall + (noteSize / 3),
-              paddingBottom: spacing.xxsmall + (noteSize / 3),
-              borderLeft: `${borderSizes.small}px solid ${uiGroups.background}`,
-              borderRight: (noteIndex === string.length - 1)
-              ? `${borderSizes.small}px solid ${uiGroups.background}`
-              : '0',
+            display: 'flex',
           }}
         >
-          <div style={{
-            height: stringIndex + 1,
-              width: fretSize,
-              background: uiGroups.background,
-              position: 'absolute',
-          }} />
-        <div style={{
-          borderRadius: '100%',
-            background: degree 
-            ? (highlightedDegreesByString[stringIndex + 1]) && (highlightedDegreesByString[stringIndex + 1].includes(degree))
-            ? uiGroups.userCurrentState
-            : uiGroups.background
-            : 'transparent',
-            width: noteSize,
-            height: noteSize,
-            zIndex: '1',
-        }} />
-    </div>
+          {string.map((degree, noteIndex) => (
+            <div
+              key={noteIndex}
+              style={{
+                width: fretSize,
+                display: 'flex',
+                position: 'relative',
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingTop: spacing.xxsmall + noteSize / 3,
+                paddingBottom: spacing.xxsmall + noteSize / 3,
+                borderLeft: `${borderSizes.small}px solid ${uiGroups.background}`,
+                borderRight: noteIndex === string.length - 1
+                  ? `${borderSizes.small}px solid ${uiGroups.background}`
+                  : '0',
+              }}
+            >
+              <div
+                style={{
+                  height: stringIndex + 1,
+                  width: fretSize,
+                  background: uiGroups.background,
+                  position: 'absolute',
+                }}
+              />
+              <div
+                style={{
+                  borderRadius: '100%',
+                  background: degree
+                    ? highlightedDegreesByString[stringIndex + 1] &&
+                        highlightedDegreesByString[stringIndex + 1].includes(
+                          degree,
+                        )
+                        ? uiGroups.userCurrentState
+                        : uiGroups.background
+                    : 'transparent',
+                  width: noteSize,
+                  height: noteSize,
+                  zIndex: '1',
+                }}
+              />
+            </div>
+          ))}
+        </div>
       ))}
     </div>
-  ))}
-</div>
   </div>
-)
+);
 
-const SelectorItem =({children}) => (
+const SelectorItem = ({ children }) => (
   <div
     style={{
       marginRight: spacing.small,
-        marginTop: spacing.small,
+      marginTop: spacing.small,
     }}
   >
     {children}
   </div>
 );
 
-const Selector = ({children, title}) => (
+const Selector = ({ children, title }) => (
   <div>
     <Heading level={4}>
       {title}
     </Heading>
 
-    <div style={{
-      display: 'flex',
+    <div
+      style={{
+        display: 'flex',
         flexWrap: 'wrap',
         marginBottom: spacing.large,
-    }}>
-    {children}
+      }}
+    >
+      {children}
+    </div>
   </div>
-  </div>
-)
+);
 
 class GuitarPatternsSelector extends Component {
-
   static defaultProps = {
     selectedDegree: 'None',
     selectedType: 'singleNotes',
-  }
+  };
 
   render() {
-    const {selectedDegree, selectedType } = this.props
+    const { selectedDegree, selectedType } = this.props;
     const highlightedDegreesByString = getHighlightedDegreesByString(
-      this.props.selectedType, 
-      this.props.selectedDegree
-    )
-    const octave = [1, 0, 2, 0, 3, 4, 0, 5, 0, 6, 0, 7]
-    const doubleOctave = octave.concat(octave)
+      this.props.selectedType,
+      this.props.selectedDegree,
+    );
+    const octave = [1, 0, 2, 0, 3, 4, 0, 5, 0, 6, 0, 7];
+    const doubleOctave = octave.concat(octave);
     const strings = [
       doubleOctave.slice(0, 13),
       doubleOctave.slice(7, 20),
@@ -180,78 +195,80 @@ class GuitarPatternsSelector extends Component {
       doubleOctave.slice(10, 23),
       doubleOctave.slice(5, 18),
       doubleOctave.slice(0, 13),
-    ]
+    ];
 
     return (
       <div>
 
-        <div style={{
-          marginBottom: spacing.medium,
-        }}>
-        We can apply the in-key pattern to the guitar. Each dot on the fretboard below is a note that is "in-key". This entire pattern repeats up and down so the ends "connect". The pattern is not specific to any note or fret number - it can be shifted up or down depending on the "key" you are in. Scroll horizontally to view the entire pattern. Tap a "Degree" and "Type" to highlight specific notes or chords inside the pattern. The most common chord progressions in typical songs use primarily the 1, 4, 5, and 6 degree chords. Most common songs will resolve back to the 1 (Major) or 6 (Minor) degree chord. Once you find the "root" of a song, you can play any of the notes in the entire in-key pattern and it is likely to sound good.
+        <div
+          style={{
+            marginBottom: spacing.medium,
+          }}
+        >
+          We can apply the in-key pattern to the guitar. Each dot on the fretboard below is a note that is 'in-key'. This entire pattern repeats up and down so the ends 'connect'. The pattern is not specific to any note or fret number - it can be shifted up or down depending on the 'key' you are in. Scroll horizontally to view the entire pattern. Tap a 'Degree' and 'Type' to highlight specific notes or chords inside the pattern. The most common chord progressions in typical songs use primarily the 1, 4, 5, and 6 degree chords. Most common songs will resolve back to the 1 (Major) or 6 (Minor) degree chord. Once you find the 'root' of a song, you can play any of the notes in the entire in-key pattern and it is likely to sound good.
+        </div>
+
+        <div
+          style={{
+            marginBottom: spacing.medium,
+          }}
+        >
+          <GuitarPattern
+            strings={strings}
+            highlightedDegreesByString={highlightedDegreesByString}
+          />
+        </div>
+
+        <Selector title='Degree'>
+          {['None', 1, 2, 3, 4, 5, 6, 7].map(degree => (
+            <SelectorItem key={degree}>
+              <Link
+                href={
+                  `/projects/guitar-lessons?type=${selectedType}&degree=${degree}`
+                }
+                scroll={false}
+              >
+                <Button
+                  type={selectedDegree === degree ? 'primary' : 'secondary'}
+                >
+                  {startCase(degree)}
+                </Button>
+              </Link>
+            </SelectorItem>
+          ))}
+        </Selector>
+
+        <Selector title='Type'>
+          {[
+            'singleNotes',
+            'powerChords',
+            'basicChords',
+            'seventhChords',
+            'ninthChords',
+            'eleventhChords',
+            'quartalChords',
+          ].map(type => (
+            <SelectorItem key={type}>
+              <Link
+                href={
+                  `/projects/guitar-lessons?type=${type}&degree=${selectedDegree}`
+                }
+                scroll={false}
+              >
+                <Button type={selectedType === type ? 'primary' : 'secondary'}>
+                  {startCase(type)}
+                </Button>
+              </Link>
+            </SelectorItem>
+          ))}
+        </Selector>
+
       </div>
-
-      <div style={{
-        marginBottom: spacing.medium,
-      }}>
-      <GuitarPattern 
-        strings={strings}
-        highlightedDegreesByString={highlightedDegreesByString}
-      />
-    </div>
-
-    <Selector
-      title='Degree'
-    >
-      {['None', 1, 2, 3, 4, 5, 6, 7].map((degree) => (
-        <SelectorItem key={degree}>
-          <Link
-            href={`/projects/guitar-lessons?type=${selectedType}&degree=${degree}`}
-            scroll={false}
-          >
-            <Button
-              type={selectedDegree === degree ? 'primary' : 'secondary'}
-            >
-              {startCase(degree)}
-            </Button>
-          </Link>
-        </SelectorItem>
-      ))}
-    </Selector>
-
-    <Selector
-      title='Type'
-    >
-      {[
-        'singleNotes',
-        'powerChords',
-        'basicChords',
-        'seventhChords',
-        'ninthChords',
-        'eleventhChords',
-        'quartalChords',
-      ].map((type) => (
-        <SelectorItem key={type}>
-          <Link
-            href={`/projects/guitar-lessons?type=${type}&degree=${selectedDegree}`}
-            scroll={false}
-          >
-            <Button
-              type={selectedType === type ? 'primary' : 'secondary'}
-            >
-              {startCase(type)}
-            </Button>
-          </Link>
-        </SelectorItem>
-      ))}
-    </Selector>
-
-  </div>
-    )
+    );
   }
 }
 
-const GuitarLessonIndex = ({ selectedDegree, selectedType}) => (
+const GuitarLessonIndex = ({ selectedDegree, selectedType }) => (
   <Screen
     title='Guitar lessons'
     description='Diatonic (in-key) chord and scale diagrams to help you go beyond tabs and memorizing songs'
@@ -261,38 +278,42 @@ const GuitarLessonIndex = ({ selectedDegree, selectedType}) => (
         component: (
           <div>
 
-            <div style={{
-              marginBottom: spacing.medium,
-            }}>
-            In common music, most notes used are "in-key". Then, chords (multiple notes played at a time) are built from these in-key notes. There are only twelve notes in music, but they repeat in "octaves". Only seven of these notes are in-key. Below is what the pattern of in-key notes looks like. Scroll horizontally to view the entire pattern. This pattern repeats infinitely up and down so the ends "connect".
+            <div
+              style={{
+                marginBottom: spacing.medium,
+              }}
+            >
+              In common music, most notes used are 'in-key'. Then, chords (multiple notes played at a time) are built from these in-key notes. There are only twelve notes in music, but they repeat in 'octaves'. Only seven of these notes are in-key. Below is what the pattern of in-key notes looks like. Scroll horizontally to view the entire pattern. This pattern repeats infinitely up and down so the ends 'connect'.
+            </div>
+
+            <GuitarPattern
+              strings={[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1]]}
+              highlightedDegreesByString={{
+                1: [1, 3, 5, 6, 8, 10, 12],
+              }}
+            />
+
           </div>
-
-          <GuitarPattern 
-            strings={[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1]]}
-            highlightedDegreesByString={{
-              1: [1, 3, 5, 6, 8, 10, 12],
-            }}
-          />
-
-      </div>
         ),
       },
       {
         title: 'The in-key guitar pattern',
         component: (
-          <GuitarPatternsSelector 
-            selectedDegree={isNaN(selectedDegree) ? selectedDegree : parseInt(selectedDegree)}
-            selectedType={selectedType} 
+          <GuitarPatternsSelector
+            selectedDegree={
+              isNaN(selectedDegree) ? selectedDegree : parseInt(selectedDegree)
+            }
+            selectedType={selectedType}
           />
         ),
       },
     ]}
   />
-)
+);
 
-GuitarLessonIndex.getInitialProps = ({query: { degree, type}}) => ({
-  selectedDegree: degree, 
+GuitarLessonIndex.getInitialProps = ({ query: { degree, type } }) => ({
+  selectedDegree: degree,
   selectedType: type,
-})
+});
 
 export default GuitarLessonIndex;
